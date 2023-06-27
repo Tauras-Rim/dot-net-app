abe364be400a7e9bc38fdb6128049d14163a2ad7
Tauras Rimdžius <tvikis3223@gmail.com>
2023-06-27 15:28:57 +0300

    Add project files.

---
+++ dot-net-example/Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace dot_net_example.Models
+{
+    public class Customer
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public string? email { get; set; }
+        public int age { get; set; }
+    }
+}
---
+++ dot-net-example/Models/CustomerContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace dot_net_example.Models
+{
+    public class CustomerContext : DbContext
+    {
+        public CustomerContext(DbContextOptions<CustomerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; } = null;
+    }
+}
